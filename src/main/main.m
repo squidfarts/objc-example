c2cec2e3e940f6601cf701cefdb2bdeb106a5fb7
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 17:47:19 -0700

    Add files via upload

---
+++ src/main/main.m
@@ -0,0 +1,32 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.m                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+#import <stdio.h>
+#import <stdlib.h>
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Objective-C' is desplayed to the standerd console.
+ *
+ */
+int main(void)
+{
+    @autoreleasepool
+    {
+        NSLog(@"Hello, Objective-C.");
+    }
+    
+    return EXIT_SUCCESS;
+}// end of function main
